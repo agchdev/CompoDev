24c27459bd82f26fa89a70c694b93aebcbd2ef9c
unknown <alex.chinchi02@gmail.com>
2025-03-05 14:03:40 +0100

    cambios

--- compodev.sql
+++ compodev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-02-2025 a las 14:51:44
+-- Tiempo de generación: 05-03-2025 a las 14:03:14
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,6 +24,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `proyectos`
+--
+
+CREATE TABLE `proyectos` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `html` longtext NOT NULL,
+  `css` longtext NOT NULL,
+  `js` longtext NOT NULL,
+  `descripcion` varchar(500) NOT NULL,
+  `categoria` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proyectos_usuarios`
+--
+
+CREATE TABLE `proyectos_usuarios` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_proyecto` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `rol` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -40,11 +69,30 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `user`, `email`, `password`, `urlFoto`) VALUES
-(1, 'agch', 'agch@gmail.com', '82bb78b29f8b9245de2e50fa1d5eda82', 'backend\\uploads\\deafult.jpg');
+(1, 'agch', 'agch@gmail.com', '82bb78b29f8b9245de2e50fa1d5eda82', 'backend\\uploads\\deafult.jpg'),
+(2, 'pepe', 'pepe@pepe.com', '$2y$10$6MPWTsCDFVLVA.lu0yoKW.zj.C3KpaKAlPFiBGRuTeODYhUvY7FKq', './uploads/pepe.jpg'),
+(3, 'pablo', 'pablo@pablo.com', '$2y$10$GBd9siKt8qk5Rmzyj3IWKewJATpikNWpi97lrI/YMh/I/1e2b3kly', './uploads/pablo.jpg'),
+(4, 'aa', 'aa@aa', '$2y$10$Z5NmY5oQVA.4Aik8YcPT7e0mYPVg.mciZutMgtkRtY5NpZI680bx2', './uploads/aa.jpg'),
+(5, 'ew', 'ew@ew', '$2y$10$OSlIKIvh9CCiAjYuBZK4aOpGuVZNa5QXVHLbkunUZlrrLP0ZBQsnO', './uploads/ew.jpg'),
+(6, 'nasa', 'nasa@nasa.com', '$2y$10$/ICzHeum6A0UrkMdgy08YONaGNTSGlLjaeYX/r9QmDPDc9yzwar2G', './uploads/nasa.jpg');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `proyectos`
+--
+ALTER TABLE `proyectos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indices de la tabla `proyectos_usuarios`
+--
+ALTER TABLE `proyectos_usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -58,10 +106,22 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `proyectos`
+--
+ALTER TABLE `proyectos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proyectos_usuarios`
+--
+ALTER TABLE `proyectos_usuarios`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
